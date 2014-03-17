a36493a6939ba3894e566911107495db1a6117f7
hendrikwiersma <hendrikwiersma@gmail.com>
2014-03-17 23:41:51 +0100

    Added project files.

---
+++ src/rushhour/RushHour.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rushhour;
+
+import java.awt.Color;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author gyrth
+ */
+public class RushHour {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        RushHourFrame1 newframe = new RushHourFrame1();
+        newframe.setVisible(true);
+        
+    }
+    
+}
